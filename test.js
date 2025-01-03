578fecdcfa4bdf5a5ad1e6610c157377e493286c
Aditya-Singh1 <adisingh0100@gmail.com>
2025-01-03 17:16:47 +0530

    yes

---
+++ test.js
@@ -0,0 +1,49 @@
+
+//  java script is a clint side and server side objective oriented script
+// lang witch is used to create web application and desktop application
+//also used to create android app using js liabrary;
+
+// feature 
+// 1.Server side(backend) and client side(frontend)supportiv
+// 2.Easily integreted with html-css
+// 3.Dynamically typed language 
+// 4.Event-driven programming.
+// 5.Synchronous and asynchrounous(async)
+// 6.By default single treaded and also used as multithreading
+// 7.Top liabraries :React js , vue js , angular js , electronic js; 
+
+//Type of appliyeing java script 
+// 1.internal java script;
+// 2.external
+
+// let => let declaire one time not declare two time same name we can override the value; 
+// var => declare two time same name and we can asign the value ;  
+// const=>not declare tow time as same name and we can not override the value ;
+
+
+let a="this is a java script";
+
+document.write(`<h1>${a}</h1>`);
+var b ="Java Script is script language";
+
+ document.write(`${a}`);
+
+document.getElementById('o').style.textAlign="center";
+document.getElementById('d1').style.width="200px";
+document.getElementById('d1').style.height="200px";
+document.getElementById('d1').style.backgroundColor="blue";
+document.getElementById('d2').style.width="200px";
+document.getElementById('d2').style.height="200px";
+document.getElementById('d2').style.backgroundColor="red";
+
+document.getElementById('d3').style.width="200px";
+document.getElementById('d3').style.height="200px";
+document.getElementById('d3').style.backgroundColor="green";
+document.getElementById('o').style.textAlign="right";
+document.getElementById('o').innerHTML="this java script"
+ 
+ 
+
+
+ 
+
