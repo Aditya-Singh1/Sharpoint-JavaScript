099e69f30484ef20ab406de9b0d4d625b0e77393
Aditya-Singh1 <adisingh0100@gmail.com>
2025-01-04 16:14:14 +0530

    yes

---
+++ 4jan-b55-24.js
@@ -0,0 +1,15 @@
+
+let name1="All freinds"
+document.write(`<h1>Hello Java</h1>`);
+document.write(`<h1>${name1}</h1>`)
+console.log("Aditya")
+let nu1=78,num2=45;
+let res=nu1+num2;
+
+document.write(`<h1 style="background-color:green"><span style="color:red"> ${nu1}</span>+ ${num2}=${res}</h1>`)
+
+let n1 =Number( prompt("enter the first n1"))
+let n2= Number(prompt("enter the num2 "))
+
+let r=n1+n2;
+document.write(`result: ${r}`)
