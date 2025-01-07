1e6d4ac5db9babce1f926a7e4ed18da53c451df8
Aditya-Singh1 <adisingh0100@gmail.com>
2025-01-07 16:21:01 +0530

    ad

---
+++ 7jan-b55.js
@@ -0,0 +1,129 @@
+//Arithmatic Oprator
+
+// let n1=45;
+// let n2=45;
+// let res=n1+n2;
+// document.write(`Add:${n1}+${n2}= ${res}`)
+
+//Subtrac
+// let n1=45
+// let n2=23;
+// let res=n1-n2;
+// document.write(`Subtrac:${n1}-${n2}= ${res}`);
+
+//multipilication
+// let n1=45
+// let n2=23;
+// let res=n1*n2;
+// document.write(`<h2>Mutltiply:${n1}*${n2}= ${res}</h2>`);
+
+// Division 
+// let n1=45
+// let n2=23;
+// let res=n1/n2;
+// document.write(`<h2>Division:${n1}*${n2}= ${res}</h2>`);
+
+
+// Remainder 
+// let n1=45
+// let n2=23;
+// let res=n1%n2;
+// document.write(`<h2>Remainder:${n1}%${n2}= ${res}</h2>`);
+
+//squre 
+
+// Square 
+// let n1=45
+// document.write(`<h2>Square:${n1}= ${n1**2}</h2>`);
+
+// Qube 
+// let n1=45
+// document.write(`<h2>Square:${n1}= ${n1**3}</h2>`);
+
+
+//comparision oprator
+
+// let x=50;
+// let y=67;
+// let d=x<y;
+// document.write(`<h2>${x}>${y} ${d}</h2>`)
+
+
+// let a=50;
+// let b=67;
+// let c=x>y;
+// document.write(`<h2>${a}>${b} ${c}</h2>`)
+
+// let x=50;
+// let y=67;
+// let d=x==y;
+// document.write(`<h2>${x}=${y} ${d}</h2>`)
+
+//Double Equal case 1
+
+// let x=67;
+// let y=67;
+// let d=x==y;
+// document.write(`<h2>${x}===${y} ${d}</h2>`)
+
+
+// Double Equal case2
+// let x="50";//string form 
+// let y=50;//Arithmatic form
+// let d=x==y;
+// document.write(`<h2>${x}==${y} ${d}</h2>`)
+
+// //Strong Equal
+// let x="50";
+// let y=50;
+// let d=x===y;
+// document.write(`<h2>${x}===${y} ${d}</h2>`)
+
+
+//post increament(a++)
+//pre increment(++a)
+
+//post Increment
+// let a=10;
+// n=a++;
+
+//PreIncrement
+// document.write(`value of n ${a}`)
+// let x=10;
+// y=++a;
+// document.write(`,${y}`)
+
+//logical oprator;
+
+// let a=56;
+// let b=45;
+// let c=78
+// let c1=a>20 && b>40 && b>30
+// document.write(`${a} and ${b}=${c1} `)
+
+// OR(||) Oprators
+// let a=56;
+//  let b=45;
+//  let c=78
+//  let c1=a<20 || b<40 || b>30
+//  document.write(`${a} and ${b}=${c1} `)
+
+
+// not oprator(!)
+
+let a=56;
+let b=45;
+let c=78
+let c1=a!=b && b>40 && b>30
+document.write(`${a} and ${b}=${c1} `)
+
+
+
+
+
+
+
+
+
+
+
