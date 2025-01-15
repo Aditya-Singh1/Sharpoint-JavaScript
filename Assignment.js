0b649bfeb516c7e3cda685a19659ec54d7fad7df
Aditya-Singh1 <adisingh0100@gmail.com>
2025-01-15 15:20:57 +0530

    yes

--- Assignment.js
+++ Assignment.js
@@ -272,71 +272,228 @@
 //9. Menu-Driven Login System...........................
 
 //----- This is the Exit Function ---------------
-let exit=()=>{
+// let exit=()=>{
 
-    document.write("Exiting the program. Have a nice day! ");
- }
-//-------------------------------------------------
+//     document.write("Exiting the program. Have a nice day! ");
+//  }
+// //-------------------------------------------------
 
 
   
 
-let Phone_number="1234567890";
-let OTP="1234"
-let Email="user@example.com";
-let Password= "password123"
+// let Phone_number="1234567890";
+// let OTP="1234"
+// let Email="user@example.com";
+// let Password= "password123"
 
 
-document.write(`<h2> 
-    Phone_number="1234567890";<br>
-    OTP="1234"<br>
-    Email="user@example.com";<br>
-    Password= "password123"
+// document.write(`<h2> 
+//     Phone_number="1234567890";<br>
+//     OTP="1234"<br>
+//     Email="user@example.com";<br>
+//     Password= "password123"
 
 
     
-    </h2>`)
+//     </h2>`)
 
-    let inp =prompt(`Enter the Option 1 login to number & OTP <br>
-        Enter the Option 2 login with email & Password <br>
-        Enter the Option 3  to Exit
-`)
+//     let inp =prompt(`Enter the Option 1 login to number & OTP <br>
+//         Enter the Option 2 login with email & Password <br>
+//         Enter the Option 3  to Exit
+// `)
      
     
-switch(inp){
+// switch(inp){
 
     
-    case "1" :
-  Phone_number=prompt("enter mob num ")
-  OTP=prompt("Enter the OTP")
+//     case "1" :
+//   Phone_number=prompt("enter mob num ")
+//   OTP=prompt("Enter the OTP")
   
- if(Phone_number=="1234567890" && OTP=="1234"){
+//  if(Phone_number=="1234567890" && OTP=="1234"){
 
-    document.write("Display success")
-}else{
+//     document.write("Display success")
+// }else{
     
-    document.write("Error");
-}
-break;
+//     document.write("Error");
+// }
+// break;
 
-case "2":
+// case "2":
 
-Email=prompt("Enter your email");
-Password=prompt("enter Your Password");
+// Email=prompt("Enter your email");
+// Password=prompt("enter Your Password");
 
-    if(Email=="user@example.com" && Password=="password123" ){
+//     if(Email=="user@example.com" && Password=="password123" ){
         
-        document.write("Display success")
-    }else{
-        document.write("Error")
-    }
-    break;
+//         document.write("Display success")
+//     }else{
+//         document.write("Error")
+//     }
+//     break;
 
-    case "3":
-        exit();
-    break;
+//     case "3":
+//         exit();
+//     break;
 
-        default:
-        document.write(`<h1>Invali Option ! to Select a Valid Option ${validate()} </h1>`)
-}
+//         default:
+//         document.write(`<h1>Invali Option ! to Select a Valid Option ${validate()} </h1>`)
+// }
+
+
+//-------Q 10--------------------------------------
+// let question1="what is 2+2?"
+// let question2="what is 2-2?"
+// let question3="what is 2*2?"
+// let question4="what is 2/2?"
+// let question5="what is 2%2?"
+
+// let opt1=`A.5,B.1,C.4,D.3,skip`
+// let opt2=`A.1,B.0,C.4,D.3,skip`
+// let opt3=`A.5,B.4,C.3,D.2,skip`
+// let opt4=`A.3,B.2,C.1,D.5,skip`
+// let opt5=`A.5,B.0,C.1,D.3,skip`
+
+// let rightAns1="c"
+// let rightAns2="b"
+// let rightAns3="b"
+// let rightAns4="c"
+// let rightAns5="b"
+
+//  // Game State Variables
+// let totalScore =+ 0;
+// let correctAnswers =+ 0;
+// let skippedQuestions = 0;
+// let wrongAnswers =+ 0;
+// let currentQuestionIndex =+ 0;
+
+
+// function strt(){
+
+// userInput=prompt("Are you strt the game yes || no")
+
+// if(userInput==="YES" ||userInput==="yes"){
+// gameproces();
+
+// }else{
+//     document.write("Exit good by ")
+// }
+
+// }
+
+
+// let count=+1;
+// function gameproces(){
+
+// if(currentQuestionIndex<count){
+
+// count++;
+
+// let userAns=prompt(`${question1}\n ${opt1} skip to skip`);
+// if(userAns==="skip" || userAns=="c" ){
+
+//     skippedQuestions++;  
+//    //document.write('skip question')
+
+//    //---------2 Q
+   
+//    userAns=prompt(`${question2}\n${opt2}`)
+//    if(userAns==="b"||userAns==="B"){
+//    correctAnswers++;
+//    totalScore+=rightAns2=2000;
+//    //document.write("correct Ans")
+   
+//    }else if(userAns==="skip"){
+//    skippedQuestions++;
+//    //document.write(`skip question ?`);
+//    }else{
+   
+//        wrongAnswers++;
+//       // document.write("wrong ans <br>")
+//    }
+   
+//    //-----------Q3
+   
+//    userAns=prompt(`${question3}\n${opt3}`)
+//    if(userAns==="b"||userAns==="B"){
+//    correctAnswers++;
+//    totalScore+=rightAns3=3000;
+   
+   
+//    }else if(userAns==="skip"){
+//    skippedQuestions++;
+//    //document.write(`skip question ?<br>`);
+//    }else{
+   
+//    wrongAnswers++;
+//    //document.write("wrong ans <br>")
+//    }
+   
+   
+//    //Q4----------------------------
+   
+//    userAns=prompt(`${question4}\n${opt4}`)
+//    if(userAns==="c"||userAns==="C"){
+//    correctAnswers++;
+//    totalScore+=rightAns4=5000;
+   
+   
+//    }else if(userAns==="skip"){
+//    skippedQuestions++;
+//    //document.write(`skip question ? <br>`);
+//    }else{
+   
+//    wrongAnswers++;
+//    //document.write("wrong ans <br>")
+//    }
+   
+   
+   
+//    //--------Q5---
+   
+   
+//    userAns=prompt(`${question5}\n${opt5}`)
+//    if(userAns==="B"||userAns==="B"){
+//    correctAnswers++;
+//    totalScore+=rightAns3=10000;
+   
+   
+//    }else if(userAns==="skip"){
+//    skippedQuestions++;
+//    //document.write(`skip question ? <br>`);
+//    }else{
+   
+//    wrongAnswers++;
+//    //document.write("wrong ans <br>")
+//    }
+//     endgame();
+// }
+ 
+// } 
+
+
+// function endgame(){
+
+//     let rating=parseInt(prompt("give your rating this game "))
+
+// if(rating>=0 && rating<=5){
+
+// document.write(`<h4>Skipped Question:${skippedQuestions}<br>
+//    correct Ans: ${correctAnswers}<br>
+//    wrong Ans:${wrongAnswers}<br>
+//    totalScore: ${totalScore}<br>
+//    rating: ${rating}<br>
+//   </h1>
+//     `)
+// }
+
+// }
+
+// }
+// strt();
+
+
+
+
+
 
---
+++ GroceryStote.js
@@ -0,0 +1,72 @@
+document.write(`
+    
+    
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+
+        .b{width:50%;height:auto;margin: auto;}
+    </style>
+</head>
+<body>
+  
+
+<div class="b">
+    
+    <hr>
+<h1 style="text-align:center">🛒Grocery Store</h1>
+<hr>
+<legend style="font-size: 40px;">------------------🧾Receipt-----------------</legend>
+ 
+<table border="1" width="100%" cellpadding="10" >
+    <tr>
+         <th>🎁Items</th>
+         <th>🎁Quantity</th>
+    </tr>
+    <tr>
+        <td>🥛Milk</td>
+        <td style="text-align:right">2</td>
+    </tr>
+    <tr>
+        <td>🍞Bread</td>
+        <td style="text-align:right">1</td>
+    </tr>
+    <tr>
+        <td>🥚Eggs</td>
+        <td style="text-align:right">12</td>
+    </tr>
+    <tr>
+        <td>🍚Rice</td>
+        <td style="text-align:right">5</td>
+    </tr>
+    <tr>
+        <td>🧈butter</td>
+        <td style="text-align:right">1</td>
+    </tr>
+    <tr>
+        <td>🍎Apples</td>
+        <td style="text-align:right">6</td>
+    </tr>
+    <tr>
+        <td><b>Total</b></td>
+        <td style="text-align:right"><b>27 </b></td>
+    </tr>
+</table>
+<hr>
+<h2 style="text-align:right">💲Total Cost 27.00</h2>
+<h3 style="text-align: center;"> 🌟thank you for shopping with us!😊</h3>
+  
+</div>
+
+</body>
+</html>
+    
+    
+    
+    
+    
+    `)
